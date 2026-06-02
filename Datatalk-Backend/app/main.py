d4a3f3d115a54f7ef04d2767d987235e86466590
Geethika Nagasri Tammineni <tamminenigeethika9@gmail.com>
2026-06-02 11:32:53 +0530

    fix: add vercel URL to CORS

--- Datatalk-Backend/app/main.py
+++ Datatalk-Backend/app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://data-talk-ten.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
